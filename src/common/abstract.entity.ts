528a6354a93c3936d7bf00b52d2a012467be99a1
Nairi <nairi.abgaryan@gmail.com>
2020-02-13 02:02:00 +0400

    FIX. Change required createdAt and updatedAt fields

--- src/common/abstract.entity.ts
+++ src/common/abstract.entity.ts
@@ -14,11 +14,11 @@ export abstract class AbstractEntity<T extends AbstractDto = AbstractDto> {
         type: 'timestamp without time zone',
         name: 'created_at',
     })
-    createdAt: Date;
+    createdAt?: Date;
 
     @UpdateDateColumn({
         type: 'timestamp without time zone',
         name: 'updated_at',
     })
-    updatedAt: Date;
+    updatedAt?: Date;
 }
